018efecede953dbc33c93393074e69363b9d2214
Shadow <pieroramirez810@gmail.com>
2024-07-21 18:16:38 -0500

    chore: with errors

---
+++ src/modules/unicodeIcons.mjs
@@ -0,0 +1,99 @@
+
+export const unicodeIcons = {
+  /**
+   * 🍎
+   */
+  RED_APPLE: "&#x1F34E;",
+  /**
+   * 🍏
+   */
+  GREEN_APPLE: "&#x1F34F;",
+  /**
+   * 💙
+   */
+  BLUE_HEART: "&#x1F499;",
+  /**
+   * 💚
+   */
+  GREEN_HEART: "&#x1F49A;",
+  /**
+   * 💛
+   */
+  YELLOW_HEART: "&#x1F49B;",
+  /**
+   * 💜
+   */
+  PURPLE_HEART: "&#x1F49C;",
+  /**
+   * 📗
+   */
+  GREEN_BOOK: "&#x1F4D7;",
+  /**
+   * 📘
+   */
+  BLUE_BOOK: "&#x1F4D8;",
+  /**
+   * 📙
+   */
+  ORANGE_BOOK: "&#x1F4D9;",
+  /**
+   * 🔴
+   */
+  LARGE_RED_CIRCLE: "&#x1F534;",
+  /**
+   * 🔵
+   */
+  LARGE_BLUE_CIRCLE: "&#x1F535;",
+  /**
+   * 🔶
+   */
+  LARGE_ORANGE_DIAMOND: "&#x1F536;",
+  /**
+   * 🔷
+   */
+  LARGE_BLUE_DIAMOND: "&#x1F537;",
+  /**
+   * 🔸
+   */
+  SMALL_ORANGE_DIAMOND: "&#x1F538;",
+  /**
+   * 🔹
+   */
+  SMALL_BLUE_DIAMOND: "&#x1F539;",
+  /**
+   * 🔺
+   */
+  UP_POINTING_RED_TRIANGLE: "&#x1F53A;",
+  /**
+   * 🔻
+   */
+  DOWN_POINTING_RED_TRIANGLE: "&#x1F53B;",
+  /**
+   * 🔼
+   */
+  UP_POINTING_SMALL_RED_TRIANGLE: "&#x1F53C;",
+  /**
+   * 🔽
+   */
+  DOWN_POINTING_SMALL_RED_TRIANGLE: "&#x1F53D;",
+  /**
+   * 🚀
+   */
+  ROCKET: "&#x1F680;",
+  /**
+   * 🚁
+   */
+  HELICOPTER: "&#x1F681;",
+  /**
+   * ∑
+   */
+  SUM: "&sum;",
+  /**
+   * 🟢
+   */
+  LARGE_GREEN_CIRCLE: "&#x1F7E2;",
+  /**
+   * ❌
+   */
+  CROSS: "&#x274C;",
+};
